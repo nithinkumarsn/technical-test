2c37d609b9a2573c705f09c8434ce709d3e8eaa5
bablu <nithinkumarsn21@gmail.com>
2020-07-28 12:28:30 +0530

    Updates

---
+++ precache-manifest.e9e4499249ccd59492c8ac04b9baca96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cccbf8b7ff75c61d702ff5e139e48c0",
+    "url": "/technical-test/index.html"
+  },
+  {
+    "revision": "47705d273e95ad5e5a75",
+    "url": "/technical-test/static/css/main.a3bf06eb.chunk.css"
+  },
+  {
+    "revision": "0035203fd684fca07cfd",
+    "url": "/technical-test/static/js/2.73d7f45a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/technical-test/static/js/2.73d7f45a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47705d273e95ad5e5a75",
+    "url": "/technical-test/static/js/main.37b96fc7.chunk.js"
+  },
+  {
+    "revision": "77de7a1fdf6636c785c2",
+    "url": "/technical-test/static/js/runtime-main.ca07af7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/technical-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
